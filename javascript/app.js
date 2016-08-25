cf6b2a2539a7b4da4d74e1f07e3c876f12f23c32
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-25 12:14:25 -0700

    adds a large number of moving points
    https://egghead.io/lessons/javascript-animating-webgl-vertices

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,6 @@
 var gl, shaderProgram, vertices;
-
+var vertices = [];
+var VERTEX_COUNT = 5000;
 
 initGL();
 createShaders();
@@ -28,30 +29,38 @@ function createShaders() {
 }
 
 function createVertices() {
-  vertices = [
-    -0.9, -0.9, 0.0,
-     0.9, -0.9, 0.0,
-     0.0,  0.9, 0.0
-  ];
+  for (var i = 0; i < VERTEX_COUNT; i++) {
+    vertices.push(Math.random() * 2 - 1);
+    vertices.push(Math.random() * 2 - 1);
+  }
 
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.DYNAMIC_DRAW)
 
   var coords = gl.getAttribLocation(shaderProgram, 'coords');
   // gl.vertexAttrib3f(coords, 0.5, 0, 0);
-  gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
+  gl.vertexAttribPointer(coords, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(coords);
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  // gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
   var pointSize = gl.getAttribLocation(shaderProgram, 'pointSize');
-  gl.vertexAttrib1f(pointSize, 100.0);
+  gl.vertexAttrib1f(pointSize, 3.0);
 
   var color = gl.getUniformLocation(shaderProgram, 'color');
   gl.uniform4f(color, 0, 1.0, 0.0, 1.0);
 }
 
 function draw() {
+  for (var i = 0; i < VERTEX_COUNT * 2; i += 2) {
+    vertices[i + 0] += Math.random() * 0.004 - 0.002;
+    vertices[i + 1] += Math.random() * 0.004 - 0.002;
+  }
+
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(vertices))
+
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.drawArrays(gl.POINTS, 0, VERTEX_COUNT);
+
+  requestAnimationFrame(draw)
 }
