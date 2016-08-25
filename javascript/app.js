cc363ca9eebc6ab582bccf5af95cce3ca540fa49
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-24 22:10:53 -0700

    makes a colored square
    https://egghead.io/lessons/javascript-setting-up-webgl?course=create-3d-
    graphics-in-javascript-using-webgl

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+var gl;
+
+initGL();
+draw();
+
+function initGL() {
+  var canvas = document.getElementById("canvas");
+  gl = canvas.getContext("webgl");
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1, 0, 0, 1);
+  gl.clear
+}
+
+function draw() {
+  gl.clear(gl.COLOR_BUFFER_BIT)
+}
