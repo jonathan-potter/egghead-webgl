8bc01d1c1e682cde6fac0a378c9444973abf8227
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-24 22:31:39 -0700

    adds a square to the center of the canvas
    https://egghead.io/lessons/javascript-introduction-to-webgl-shaders

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,8 @@
-var gl;
+var gl, shaderProgram;
+
 
 initGL();
+createShaders();
 draw();
 
 function initGL() {
@@ -8,10 +10,40 @@ function initGL() {
   gl = canvas.getContext("webgl");
 
   gl.viewport(0, 0, canvas.width, canvas.height);
-  gl.clearColor(1, 0, 0, 1);
-  gl.clear
+  gl.clearColor(1, 1, 1, 1);
+}
+
+function createShaders() {
+  var vs = `
+    void main() {
+      gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+      gl_PointSize = 10.0;
+    }
+  `;
+
+  var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vs);
+  gl.compileShader(vertexShader);
+
+  var fs = `
+    void main() {
+      gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
+  `;
+
+  var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fs);
+  gl.compileShader(fragmentShader);
+
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
 }
 
 function draw() {
-  gl.clear(gl.COLOR_BUFFER_BIT)
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.POINTS, 0, 1);
 }
