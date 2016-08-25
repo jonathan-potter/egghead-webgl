d0b54b7bd5a70fc8dbcfb46df1b3775d588137ae
Jonathan Potter <jonathan.potter.business@gmail.com>
2016-08-25 12:42:17 -0700

    adds user interaction
    https://egghead.io/lessons/javascript-user-interaction-in-webgl

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,17 @@
-var gl, shaderProgram, vertices;
-var vertices = [];
-var VERTEX_COUNT = 5000;
+var gl, shaderProgram, vertices,
+    vertices = [],
+    VERTEX_COUNT = 5000,
+    mouseX = 0;
+    mouseY = 0;
+
+canvas.addEventListener('mousemove', function (event) {
+  mouseX = map(event.clientX, 0, canvas.width,  -1,  1);
+  mouseY = map(event.clientY, 0, canvas.height,  1, -1);
+})
+
+function map(value, minSrc, maxSrc, minDst, maxDst) {
+  return (value - minSrc) / (maxSrc - minSrc) * (maxDst - minDst) + minDst;
+}
 
 initGL();
 createShaders();
@@ -53,8 +64,21 @@ function createVertices() {
 
 function draw() {
   for (var i = 0; i < VERTEX_COUNT * 2; i += 2) {
-    vertices[i + 0] += Math.random() * 0.004 - 0.002;
-    vertices[i + 1] += Math.random() * 0.004 - 0.002;
+    var x = vertices[i + 0];
+    var y = vertices[i + 1];
+
+    var dx = x - mouseX;
+    var dy = y - mouseY;
+
+    var dist = Math.sqrt(dx * dx + dy * dy);
+
+    if (dist < 0.2) {
+      vertices[i + 0] = x + dx * 0.1;
+      vertices[i + 1] = y + dy * 0.1;
+    } else {
+      vertices[i + 0] = x + Math.random() * 0.004 - 0.002;
+      vertices[i + 1] = y + Math.random() * 0.004 - 0.002;
+    }
   }
 
   gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(vertices))
