d3fad86fa9e2e0ba3f38b94c80169af916a97240
Jonathan Potter <jonathan@RawrBox.local>
2016-12-20 21:04:51 -0800

    triangle\!

---
+++ app.js
@@ -0,0 +1,57 @@
+let gl,
+    shaderProgram,
+    vertices
+
+initGL()
+createShader()
+createVertices()
+draw()
+
+function initGL() {
+  const canvas = document.getElementById('canvas')
+
+  gl = canvas.getContext('webgl')
+
+  gl.viewport(0, 0, canvas.width, canvas.height)
+  gl.clearColor(1, 1, 1, 1);
+}
+
+function createShader() {
+  let vertexShader = getShader(gl, "shader-vs")
+  let fragmentShader = getShader(gl, "shader-fs")
+
+  shaderProgram = gl.createProgram()
+  gl.attachShader(shaderProgram, vertexShader)
+  gl.attachShader(shaderProgram, fragmentShader)
+  gl.linkProgram(shaderProgram)
+  gl.useProgram(shaderProgram)
+}
+
+function createVertices () {
+  vertices = [
+    -0.9, -0.9, 0.0,
+     0.9, -0.9, 0.0,
+     0.0,  0.9, 0.0
+  ]
+
+  var buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+
+  var coords = gl.getAttribLocation(shaderProgram, "coords")
+  // gl.vertexAttrib3f(coords, 0, 0, 0)
+  gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0)
+  gl.enableVertexAttribArray(coords)
+  gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
+  var pointSize = gl.getAttribLocation(shaderProgram, "pointSize")
+  gl.vertexAttrib1f(pointSize, 10)
+
+  var color = gl.getUniformLocation(shaderProgram, "color")
+  gl.uniform4f(color, 0, 1, 0, 1)
+}
+
+function draw() {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.TRIANGLES, 0, 3)
+}
